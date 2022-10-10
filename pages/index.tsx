37162702cf1ad5ecff75ed4f0e06c1ec269a31f1
whatzittoya <whosendall.com>
2022-10-10 13:41:37 +0700

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
 const posts = [
   { title: "React Testing", excerpt: "Learn React Testing" },
   { title: "React with Tailwind", excerpt: "Learn React Tailwind" },
@@ -14,14 +15,14 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((posts, index) => (
-            <div>
-              {posts.title}
-              {posts.excerpt}
-            </div>
+            <PostCard post={posts} key={posts.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8"></div>
+          <div className="lg:sticky relative top-8">
+            <PostWidget></PostWidget>
+            <Categories></Categories>
+          </div>
         </div>
       </div>
     </div>
